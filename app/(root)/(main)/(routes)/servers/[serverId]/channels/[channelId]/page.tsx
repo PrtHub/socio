45016a0c5c98965cc3026f40e47f21dc0f965882
Pritam <pri10145@gmail.com>
2024-10-31 14:01:18 +0530

    chat input component

--- app/(root)/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(root)/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { currentProfile } from "@/lib/current-profile";
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 
 const ChannelIdPage = async ({
   params,
@@ -37,6 +38,13 @@ const ChannelIdPage = async ({
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        type="channel"
+        name={channel.name}
+        apiUrl="/api/socket/messages"
+        query={{ channelId: channel.id, serverId: channel.serverId }}
       />
     </div>
   );
