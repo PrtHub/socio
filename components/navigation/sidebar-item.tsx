2cc3db9e60316cbe83450fd72940191b765cbb99
Pritam <pri10145@gmail.com>
2024-11-04 20:51:13 +0530

    error fixes

--- components/navigation/sidebar-item.tsx
+++ components/navigation/sidebar-item.tsx
@@ -24,14 +24,14 @@ const SidebarItem = ({ id, name, icon }: SidebarItemProps) => {
         <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
-            params.serverId !== id && "group-hover:h-[20px]",
-            params.serverId === id ? "h-[36px]" : "h-[8px]"
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params?.serverId === id ? "h-[36px]" : "h-[8px]"
           )}
         />
         <div
           className={cn(
             "group relative w-[48px] h-[48px] mx-3 flex items-center justify-center rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
-            params.serverId === id &&
+            params?.serverId === id &&
               "bg-primary/10 text-primary rounded-[16px]"
           )}
         >
